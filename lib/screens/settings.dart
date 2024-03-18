5e8c906cddd9eff73846da7151d372b5f7e4ec63
Hogix <92804727+Hogix@users.noreply.github.com>
2024-03-18 21:40:13 +0100

    Update settings.dart

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,15 +1,16 @@
-import 'package:app_lock_flutter/executables/controllers/method_channel_controller.dart';
-import 'package:app_lock_flutter/executables/controllers/password_controller.dart';
-import 'package:app_lock_flutter/screens/pin.dart';
-import 'package:app_lock_flutter/services/constant.dart';
-import 'package:app_lock_flutter/widgets/confirmation_dialog.dart';
-import 'package:app_lock_flutter/widgets/pass_confirm_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:app_lock_flutter/screens/pin.dart';
+import 'package:app_lock_flutter/widgets/confirmation_dialog.dart';
+import 'package:app_lock_flutter/widgets/pass_confirm_dialog.dart';
+import 'package:app_lock_flutter/widgets/main_apptimer.dart';
+
+import '../executables/controllers/method_channel_controller.dart';
+import '../executables/controllers/password_controller.dart';
+import '../services/constant.dart'; // Import du fichier contenant la classe MyApp
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key}) : super(key: key);
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -27,50 +28,66 @@ class _SettingsPageState extends State<SettingsPage> {
         child: Center(
           child: Column(
             children: [
-              ElevatedButton(onPressed: () {
-                    if (Get.find<PasswordController>()
-                        .prefs
-                        .containsKey(AppConstants.setPassCode)) {
-                      showComfirmPasswordDialog(context).then((value) {
-                        if (value as bool) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const PinScreen(),
-                            ),
-                          );
-                        }
-                      });
-                    } else {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const PinScreen(),
-                        ),
-                      );
-                    }
-                  }, child: Text("Set Passcode")),
               ElevatedButton(
-                  onPressed: () async {
-                    await showGeneralDialog(
-                      context: context,
-                      barrierDismissible: false,
-                      barrierLabel: MaterialLocalizations.of(context)
-                          .modalBarrierDismissLabel,
-                      transitionDuration: const Duration(milliseconds: 200),
-                      pageBuilder: (context, animation1, animation2) {
-                        return const ConfirmationDialog(
-                            heading: "Wait",
-                            bodyText:
-                                "Are you sure that you want to stop the process?");
-                      },
-                    ).then((value) {
+                onPressed: () {
+                  if (Get.find<PasswordController>()
+                      .prefs
+                      .containsKey(AppConstants.setPassCode)) {
+                    showComfirmPasswordDialog(context).then((value) {
                       if (value as bool) {
-                        Get.find<MethodChannelController>().stopForeground();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const PinScreen(),
+                          ),
+                        );
                       }
                     });
-                  },
-                  child: Text("Kill Process"))
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const PinScreen(),
+                      ),
+                    );
+                  }
+                },
+                child: Text("Set Passcode"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await showGeneralDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    barrierLabel: MaterialLocalizations.of(context)
+                        .modalBarrierDismissLabel,
+                    transitionDuration: const Duration(milliseconds: 200),
+                    pageBuilder: (context, animation1, animation2) {
+                      return const ConfirmationDialog(
+                          heading: "Wait",
+                          bodyText:
+                          "Are you sure that you want to stop the process?");
+                    },
+                  ).then((value) {
+                    if (value as bool) {
+                      Get.find<MethodChannelController>().stopForeground();
+                    }
+                  });
+                },
+                child: Text("Kill Process"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Naviguer vers la page des temps des applications (MyApp)
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ),
+                  );
+                },
+                child: Text("App Usage"),
+              ),
             ],
           ),
         ),
