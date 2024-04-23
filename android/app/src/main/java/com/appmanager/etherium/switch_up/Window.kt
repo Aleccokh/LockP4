7eef5eca8055be6732e637d01a05773e8977e481
loukini <120371656+loukini@users.noreply.github.com>
2024-04-23 12:03:13 +0200

    Rajout exo pop

--- android/app/src/main/java/com/appmanager/etherium/switch_up/Window.kt
+++ android/app/src/main/java/com/appmanager/etherium/switch_up/Window.kt
@@ -2,121 +2,127 @@ package com.applockFlutter
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.SharedPreferences
 import android.graphics.PixelFormat
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import android.view.*
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import com.andrognito.pinlockview.IndicatorDots
-import com.andrognito.pinlockview.PinLockListener
-import com.andrognito.pinlockview.PinLockView
-import com.applockFlutter.R
+import androidx.core.widget.doAfterTextChanged
+import kotlin.random.Random
 
 
 @SuppressLint("InflateParams")
 class Window(
-		private val context: Context
+    private val context: Context
 ) {
-	private val mView: View
-	var pinCode: String = ""
-	var txtView: TextView? = null
-	private var mParams: WindowManager.LayoutParams? = null
-	private val mWindowManager: WindowManager
-	private val layoutInflater: LayoutInflater
+    private val mView: View
+    private var result: String = ""
+    private var mParams: WindowManager.LayoutParams? = null
+    private val mWindowManager: WindowManager
+    private val layoutInflater: LayoutInflater
 
-	private var mPinLockView: PinLockView? = null
-	private var mIndicatorDots: IndicatorDots? = null
-	private val mPinLockListener: PinLockListener = object : PinLockListener {
+    private var mGameResult: EditText? = null
+    private var mGameValidate: Button? = null
+    private var mGameText: TextView? = null
 
-		@SuppressLint("LogConditional")
-		override fun onComplete(pin: String) {
-			Log.d(PinCodeActivity.TAG, "Pin complete: $pin")
-			pinCode = pin
-			doneButton()
-		}
+    private var number1 = 0
+    private var number2 = 0
+    private var operation = ' '
 
-		override fun onEmpty() {
-			Log.d(PinCodeActivity.TAG, "Pin empty")
-		}
+    fun open() {
 
-		@SuppressLint("LogConditional")
-		override fun onPinChange(pinLength: Int, intermediatePin: String) {}
-	}
+        mGameResult!!.error = null
 
-	fun open() {
-		try {
-			if (mView.windowToken == null) {
-				if (mView.parent == null) {
-					mWindowManager.addView(mView, mParams)
-				}
-			}
-		} catch (e: Exception) {
-			e.printStackTrace()
-		}
-	}
+        number1 = Random.nextInt(0, 20)
+        number2 = Random.nextInt(0, 20)
+        operation = arrayOf('-', '+', 'x').random()
 
-	fun isOpen():Boolean{
-		return (mView.windowToken != null && mView.parent != null)
-	}
+        val newText = "$number1 $operation $number2 ="
+        mGameText!!.text = newText
 
-	fun close() {
-		try {
-			Handler(Looper.getMainLooper()).postDelayed({
-				(context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).removeView(mView)
-				mView.invalidate()
-			},500)
+        try {
+            if (mView.windowToken == null) {
+                if (mView.parent == null) {
+                    mWindowManager.addView(mView, mParams)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
-		} catch (e: Exception) {
-			e.printStackTrace()
-		}
-	}
+    fun isOpen(): Boolean {
+        return (mView.windowToken != null && mView.parent != null)
+    }
 
-	fun doneButton(){
-		try {
-			mPinLockView!!.resetPinLockView()
-			val saveAppData: SharedPreferences = context.getSharedPreferences("save_app_data", Context.MODE_PRIVATE)
-			val dta: String = saveAppData.getString("password", "PASSWORD")!!
-			if(pinCode == dta){
-				println("$pinCode---------------pincode")
-				close()
-			}else{
-				txtView!!.visibility = View.VISIBLE
-			}
-		} catch (e: Exception) {
-			println("$e---------------doneButton")
-		}
-	}
+    fun close() {
+        mGameResult!!.text.clear()
+        mGameResult!!.clearFocus()
+        try {
+            Handler(Looper.getMainLooper()).postDelayed({
+                (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).removeView(mView)
+                mView.invalidate()
+            }, 500)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
-	init {
+    private fun doneButton() {
+        if (result == "") {
+            mGameResult!!.error = "Réponse vide"
+        } else {
+            try {
+                var answer = 0
+                when (operation) {
+                    '-' -> answer = number1 - number2
+                    '+' -> answer = number1 + number2
+                    'x' -> answer = number1 * number2
+                }
+                if (result.toInt() == answer) {
+                    close()
+                } else {
+                    mGameResult!!.error = "Mauvaise réponse"
+                }
+            } catch (e: Exception) {
+                mGameResult!!.error = "La réponse doit être un nombre"
+            }
+        }
+    }
 
-		mParams = WindowManager.LayoutParams(
-				WindowManager.LayoutParams.MATCH_PARENT,
-				WindowManager.LayoutParams.MATCH_PARENT,
-				WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-				WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-				PixelFormat.TRANSLUCENT
-		)
-		layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-		mView = layoutInflater.inflate(R.layout.pin_activity, null)
+    init {
 
-		mParams!!.gravity = Gravity.CENTER
-		mWindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        mParams = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+            PixelFormat.TRANSLUCENT
+        )
 
-		mPinLockView = mView.findViewById(R.id.pin_lock_view)
-		mIndicatorDots = mView.findViewById(R.id.indicator_dots)
-		txtView = mView.findViewById(R.id.alertError) as TextView
+        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        mView = layoutInflater.inflate(R.layout.flutter_game, null)
 
+        mParams!!.gravity = Gravity.CENTER
+        mWindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
+        mGameResult = mView.findViewById(R.id.game_result)
+        mGameValidate = mView.findViewById(R.id.game_validate)
+        mGameText = mView.findViewById(R.id.game_text)
 
-		mPinLockView!!.attachIndicatorDots(mIndicatorDots)
-		mPinLockView!!.setPinLockListener(mPinLockListener)
-		mPinLockView!!.pinLength = 6
-		mPinLockView!!.textColor = ContextCompat.getColor(context, R.color.ic_launcher_background)
-		mIndicatorDots!!.indicatorType = IndicatorDots.IndicatorType.FILL_WITH_ANIMATION
+        mGameResult!!.doAfterTextChanged {
+            result = mGameResult!!.text.toString()
+            mGameResult!!.error = null
+        }
 
-	}
+        mGameValidate!!.setOnClickListener {
+            doneButton()
+        }
+    }
 
 }
